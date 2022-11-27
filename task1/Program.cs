4b2c114ad4c024eb26c969422f7edf17630fb407
Petr <sppetrak@mail.ru>
2022-11-27 18:20:59 +0300

    полиндром

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string num = Console.ReadLine()!;
+
+if(num.Length == 5)
+{
+    if(num[0] == num[4] && num[1] == num[3])
+    {
+        Console.Write("Введенное число является полиндромом! ");
+    }
+    else 
+    {
+        Console.Write("Введенное число не является полиндромом! ");
+    }
+}
+    else
+    {
+        Console.Write("Введено неверное число! ");
+    }
+
+
